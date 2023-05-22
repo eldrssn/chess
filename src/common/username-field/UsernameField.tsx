c8b62999ecf28e25208e42a7a41174bddf316046
eldrssn <maxkalimullin@gmail.com>
2023-05-22 22:44:12 +0530

    fix title & username's photo

--- src/common/username-field/UsernameField.tsx
+++ src/common/username-field/UsernameField.tsx
@@ -5,14 +5,14 @@ import { UsernameFieldLayout } from "layouts/username-field-layout";
 import styles from "./username-field.module.scss";
 
 export const UsernameField = () => (
-    <UsernameFieldLayout>
-        <img
-            className={styles.icon}
-            src="assets/imgs/user-icon.jpeg"
-            width={30}
-            height={30}
-            alt="username icon"
-        />
-        <p className={styles.username}>User Name</p>
-    </UsernameFieldLayout>
+  <UsernameFieldLayout>
+    <img
+      className={styles.icon}
+      src="/assets/imgs/user-icon.jpeg"
+      width={30}
+      height={30}
+      alt="username icon"
+    />
+    <p className={styles.username}>User Name</p>
+  </UsernameFieldLayout>
 );
